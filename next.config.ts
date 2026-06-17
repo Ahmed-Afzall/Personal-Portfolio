95f4c317a986fa675c1d20ce41f9864c7ca5e39a
Ahmed-Afzall <af.ahmeddd@gmail.com>
2026-06-17 20:49:41 +0530

    Configure GitHub Pages deployment via Actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // If your repo is NOT Ahmed-Afzall.github.io, you might need a basePath:
+  // basePath: '/Personal-Portfolio',
 };
 
 export default nextConfig;
